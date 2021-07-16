27ded14c5e8b52fe41914ba89d2875e9e311c709
Alice-63 <alicetindag84@gmail.com>
2021-07-16 10:59:50 +0200

    created
    html
    style
    script

---
+++ script.js
@@ -0,0 +1,23 @@
+
+$(function(){
+
+
+$("i").click(function () 
+{ 
+    $(this).css("color","red")
+        
+})
+
+
+
+// $("#container>i").mouseenter(function(){
+    
+//     $(this).css("color","red")
+// })
+// $("#container>i").mouseleave(function () {
+
+//     $(this).css("color","")
+// });
+
+
+})
